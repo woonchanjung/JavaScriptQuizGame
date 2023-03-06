9bea561fd97942ad788f0777918ef80f65f17ad1
xkrzypandax <xkrzypandax@gmail.com>
2023-03-06 08:48:50 -0800

    Created Pseudo Code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # JavaScriptQuizGame
-# JavaScriptQuizGame
+
---
+++ js/main.js
@@ -0,0 +1,64 @@
+        /*----- constants -----*/
+
+// Defines the const for Audio  
+    // Ex - const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
+
+// Defines the const for Imgs
+    // const RPS_LOOKUP = {
+    //     r: {img: 'imgs/rock.png', beats: 's'},
+    //     p: {img: 'imgs/paper.png', beats: 'r'},
+    //     s: {img: 'imgs/scissors.png', beats: 'p'},
+    // };
+
+// Defines the array of Objects for Questions
+
+// Defines the arry of Asnwer for the Questions
+
+        /*----- app's state (variables) -----*/
+
+// Defines a variable that will keep track of the Question#
+let currentQuestion;
+
+// Defines a variable that will keep track of the score
+let scores;
+
+// Defines a variable that will give the results
+let winner; 
+
+        /*----- cached element references -----*/
+
+// Includes cached elements for later usage. 
+    // Ex. const pResultEl = document.getElementById('p-result');
+
+        /*----- event listeners -----*/
+
+// Include event listeners for the answers of the users
+
+/*----- functions -----*/
+
+// Initiatlize all state, then call render();
+
+// Display the current questions and its choices of answers
+
+// In response to user interaction (player makes a move).
+
+// We update all impacted state, then finally, call render()
+
+// Guards (do nothing unless of the three buttons were clicked)
+    //if (evt.target.tagName !== 'BUTTON') return;
+
+// Player has made a choice
+
+// Let player know if the answer was correct or wrong
+
+// Keep track of the user's score
+
+// Compuet a random choice question
+
+// Transfer/visualize all state to the DOM
+
+// Repeat
+
+// Display the final score
+
+// Suggest Resources to study more.
