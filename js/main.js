b042f2984e68f9ed21cda27ec2a759f1913281a9
xkrzypandax <xkrzypandax@gmail.com>
2023-03-09 13:42:59 -0800

    Made some progress on JS

--- js/main.js
+++ js/main.js
@@ -1,60 +1,84 @@
-        /*----- constants -----*/
-
-// Defines the const for Audio  
-    // Ex - const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
-
-// Defines the const for Imgs
+/*----- constants -----*/
 
 // Defines the array of Objects for Questions
+const questions = [{
+    question: 'What is the result of typeof null?',
+    choices: ['Object', 'null', 'undefined', 'Nan'],
+    answer: 1
+}];
 
-
-// Defines the arry of Asnwer for the Questions
-
-        /*----- app's state (variables) -----*/
+/*----- app's state (variables) -----*/
 
 // Defines a variable that will keep track of the Question#
 let currentQuestion;
 
 // Defines a variable that will keep track of the score
-let scores = 0;
+let score = 0;
 
 // Defines a variable that will give the results
 let winner = false; 
 
-        /*----- cached element references -----*/
+/*----- cached element references -----*/
 
 // Includes cached elements for later usage. 
-    // Ex. const pResultEl = document.getElementById('p-result');
+const questionEl = document.getElementById('question'); 
+const buttonEl = document.getElementById('button'); 
+const scoreEl = document.getElementById('score');
+const resultEl = document.getElementById('result');
+const playAgainEl = document.getElementById('play-again')
 
-        /*----- event listeners -----*/
+/*----- event listeners -----*/
 
 // Include event listeners for the answers of the users
+choicesEl.addEventListener('click', function(event)){
+
+}
 
 /*----- functions -----*/
+init();
 
 // Initiatlize all state, then call render();
+function init() {
+    currentQuestion = 0
+    score = 0;
+    winner = null;
+    render();
+}
 
+// Visualize all state in the DOM 
+function render() {
+    renderQuestions();
+    renderAnswers();
+    renderScore();
+// Hide and show play again button
+    renderControls();
+}
+
+function renderQuestions() {
+    
+}
+
+function renderAnswers() {
+
+}
+
+function renderScore() {
+
+}
+
+function renderControls() {
+
+}
 // Display the current questions and its choices of answers
 
 // In response to user interaction (player makes a move).
 
 // We update all impacted state, then finally, call render()
 
-// Guards (do nothing unless of the three buttons were clicked)
-    //if (evt.target.tagName !== 'BUTTON') return;
-
 // Player has made a choice
 
 // Let player know if the answer was correct or wrong
 
-// Keep track of the user's score
-
-// Compuet a random choice question
-
-// Transfer/visualize all state to the DOM
+// Compute a random question
 
 // Repeat
-
-// Display the final score
-
-// Suggest Resources to study more.
